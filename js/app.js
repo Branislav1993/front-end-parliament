06913dadfa71cdd5e91f267f9087669079a6418d
Branislav1993 <branislav.vidojevic@outlook.com>
2015-10-17 13:30:05 +0200

    Added more docs, fixed some angular logic

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 /**
  * Created by Branislav Vidojevic on 15/10/2015.
  */
-var app = angular.module("app",[]);
+var app = angular.module("app", []);
 
-app.controller('memberCtrl', ['$scope', '$http', function($scope, $http){
+app.value
 
-    $http.get('members.json').success(function(data){
-        $scope.d = data;
-        var text = data.result;
-        $scope.json = JSON.stringify(JSON.parse(text),null,2);
-    }).error(function(){
-        alert('Greska!!!');
+app.controller('membersCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    $http.get('members.json').success(function (data) {
+        $scope.docs = data.docs;
+    }).error(function () {
+        console.log('Error, GET member.json failed.');
     });
 
-
+    $scope.formatJson = function (text) {
+        return JSON.stringify(JSON.parse(text), null, 2);
+    }
 }]);
